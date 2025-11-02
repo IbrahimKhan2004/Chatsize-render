89894183871b12911be375080bb20a0cf5aadabe
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2025-11-03 03:35:18 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -306,6 +306,6 @@ def welcome(_, message: Message):
     te = "🇹🇷 Esenlikler. Bir kanal/grup kimliği gönder, tüm dosyaların toplam boyutunu hesaplaycağım." \
         "\nKanaldaki / gruptaki son mesaja tıkla, mesaj bağlantısını kopyala, bana yapıştır." \
         "\n\n🇬🇧 Hi. Send a channel/group id and I will calculate the full size of all files." \
-        "\nClick the last message in the channel / group, copy the message link, paste it to me.." \
+        "\nClick the last message in the channel / group, copy the message link, paste it to me Sir.." \
         f"\n\n**@{Config.CHANNEL_OR_CONTACT}**"
     message.reply_text(te, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
