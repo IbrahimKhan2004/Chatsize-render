92e9bf3ddc40491656913e1356e32522be1e4ae1
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2026-05-10 05:10:55 +0530

    Refactor bot.py to use async main function

--- bot.py
+++ bot.py
@@ -5,23 +5,26 @@ import time
 from config import Config
 from pyrogram import idle
 import pyrogram
+import asyncio
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
     level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 botStartTime = time.time()
 
-if __name__ == '__main__':
-    # init pyrogram client
+async def main():
     plugins = dict(root = 'plugins')
     app = pyrogram.Client("ChatSizeBot", bot_token = Config.BOT_TOKEN,
         api_id = Config.APP_ID, api_hash = Config.API_HASH, plugins = plugins)
-    app.start()
-    LOGGER.info(app.get_me())
+    await app.start()
+    LOGGER.info(await app.get_me())
     LOGGER.info(msg="Bot Started.")
-    try: app.send_message(Config.OWNER_ID, "Bot Started.")
+    try: await app.send_message(Config.OWNER_ID, "Bot Started.")
     except: pass
-    idle()
+    await idle()
     LOGGER.info(msg="Bot Stopped.")
-    try: app.send_message(Config.OWNER_ID, "Bot Stopped.")
+    try: await app.send_message(Config.OWNER_ID, "Bot Stopped.")
     except: pass
+
+if __name__ == '__main__':
+    asyncio.run(main())
