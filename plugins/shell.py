fa3bb89b09b5138896b107b1cbd8144712432939
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2026-05-10 05:10:22 +0530

    Change shell function to async and update replies

--- plugins/shell.py
+++ plugins/shell.py
@@ -10,12 +10,12 @@ from subprocess import Popen, PIPE
 from pyrogram.types.messages_and_media.message import Message
 
 @Client.on_message(filters.command("shell"))
-def shell(_, message:Message):
+async def shell(_, message:Message):
     if Config.OWNER_ID == 0 or message.from_user.id != Config.OWNER_ID: return
     try:
         cmd = message.text.split(' ', 1)
         if len(cmd) == 1:
-            return message.reply_text('🇬🇧 No command to execute was given.\n\n🇹🇷 Boşluk bırakıp komut gir zırcahil seni.',
+            return await message.reply_text('🇬🇧 No command to execute was given.\n\n🇹🇷 Boşluk bırakıp komut gir zırcahil seni.',
                 reply_to_message_id = message.id)
         cmd = cmd[1].strip()
         process = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
@@ -33,9 +33,9 @@ def shell(_, message:Message):
             with open('shell.txt', 'w') as file:
                 file.write(reply)
             with open('shell.txt', 'rb') as doc:
-                message.reply_document(document=doc)
+                await message.reply_document(document=doc)
             if os.path.isfile('shell.txt'): os.remove('shell.txt')
         else:
-            message.reply_text(reply)
+            await message.reply_text(reply)
     except:
-        message.reply_text(f"🇬🇧 Maybe your shell message was empty.\n🇹🇷 Boş bir şeyler döndü valla.\n\n{reply}")
+        await message.reply_text(f"🇬🇧 Maybe your shell message was empty.\n🇹🇷 Boş bir şeyler döndü valla.\n\n{reply}")
